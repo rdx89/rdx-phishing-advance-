0137609173f6dbc7f66fbf81e0412103e9e3b8fc
Rahul Pipliwal <91361622+rdx89@users.noreply.github.com>
2023-07-07 12:06:10 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$sender_name = 'Rdx Cyber'; 
+$sender_mail = 'pipliwal.89@gmail.com';
+$sender_pass = 'majddcsxyezqobxx';
+
+$receiver_name = 'Shijuka';
+$receiver_mail = 'tecnoeffect89@gmail.com';
+
+?>
